026a1e4141c39e51b79dc28b741447a2aed6fb9c
en568 <jsmith@enormous-air.com>
2025-04-03 05:36:34 +0530

    Create config.go

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+var DB *sql.DB
+
+func InitDB() *sql.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"),
+	))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB = db
+	return db
+}
